175e2cf205fd7fca0d51f2c3104521387b96a75a
Madhumitha_G <madhumitha.g@waveaxis.com>
2024-12-30 14:20:54 +0100

    'Simplifying code'

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,20 @@
 'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
-const Page = () => {
-  const { user, isLoading } = useUser();
-  const router = useRouter();
 
-  useEffect(() => {
-    if (user) {
-      router.push('/');
-    }
-  }, [user, router]);
+export default function Index() {
+  const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-  return (
-    <div>
-      <h1>Logged out</h1>
-      <a href="/api/auth/login">Login back in</a>
-    </div>
-  );
-};
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
 
-export default Page;
+  return <a href="/api/auth/login">Login</a>;
+}
